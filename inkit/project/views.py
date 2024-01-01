6684ebc8f2d87f19ee698c4862644d5d7417983b
R0H-N <rohn.py@gmail.com>
2024-01-01 13:36:09 +0530

    initial commit after first migrate

---
+++ inkit/project/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+def project(request):
+    page = 'project'
+    number = 9
+    context={ 'page':page, 'number':number}
+    
+    return render(request,'project/project.html', context )
+
+def proj(request, pk):
+    return render(request,'project/proj.html')
