9a8d330830139f97c83cfd3b9f93bbbf68295907
R0H-N <rohn.py@gmail.com>
2024-01-27 21:27:37 +0530

    setting mailing backend , password reset done

--- inkit/users/signals.py
+++ inkit/users/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
+from django.core.mail import send_mail
+from django.conf import settings
     
 def createProfile(sender,instance,created,**kwargs):
     if created :
@@ -12,7 +14,20 @@ def createProfile(sender,instance,created,**kwargs):
             email = user.email,
             name = user.first_name,
         )
+
+        subject = 'Welcome to devsearch'
+        message_body = 'we are happy to see you!'
     
+        send_mail(
+            subject,
+            message_body,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False,
+        )
+
+
+
 
 def deleteUser(sender,instance,**kwargs):
     user = instance.user
