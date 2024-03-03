34f2295e3e4adbae82d98d0bf3221df9a826f2c6
R0H-N <rohn.py@gmail.com>
2024-03-03 15:17:29 +0530

    last few updates

--- inkit/api/views.py
+++ inkit/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated,IsAdminUser
 from rest_framework.response import Response
 from .serializers import ProjectSerializer
-from project.models import project
+from project.models import project,review,Tag
 
 @api_view(['GET'])
 def getRoutes(request):
@@ -28,4 +29,23 @@ def getProjects(request):
 def getProject(request,pk):
     proj = project.objects.get(id=pk)
     serializer = ProjectSerializer(proj,many=False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def projectReview(request,pk):
+    projects = project.objects.get(id=pk)
+    user = request.user.profile
+    data = request.data
+
+    Review,create = review.objects.get_or_create(
+        owner=user,
+        project = projects,
+    )
+    Review.value = data['value']
+    Review.save()
+    project.getVoteCount
+
+
+    serializer = ProjectSerializer(projects,many=False)
     return Response(serializer.data)
